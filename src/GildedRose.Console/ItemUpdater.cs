fbe9021b7cdc9c4df143d642c231597549ffe335
sharonrussell <sharon.russell@comparethemarket.com>
2016-03-10 13:37:41 +0000

    making backstage pass test pass

--- src/GildedRose.Console/ItemUpdater.cs
+++ src/GildedRose.Console/ItemUpdater.cs
@@ -8,7 +8,7 @@ namespace GildedRose.Console
         {
             foreach (var item in items)
             {
-                if(item.Name != "Sulfuras, Hand of Ragnaros")
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
                 {
                    UpdateQuality(item);
                    UpdateSellIn(item);
@@ -18,7 +18,7 @@ namespace GildedRose.Console
 
         private void UpdateQuality(Item item)
         {
-            if (item.Name == "Aged Brie")
+            if (item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
                 IncreaseQuality(item);
             }
@@ -47,8 +47,18 @@ namespace GildedRose.Console
 
         private void IncreaseQuality(Item item)
         {
-            if (item.Quality < 50)
+            if (item.Quality >= 50) return;
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (item.SellIn >= 10)
+                {
+                    item.Quality += 2;
+                }
+            }
+            else
+            {
                 item.Quality += 1;
+            }
         }
     }
 }
